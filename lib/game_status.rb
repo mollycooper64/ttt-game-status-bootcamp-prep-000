4e6f4690587ce61149a0cea5a596bdfa86105611
Molly Cooper <mollycooper64@gmail.com>
2020-05-16 18:41:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,7 +27,7 @@ def won?(board)
 
     board[location1] == board[location2] && board[location2] == board[location3] && board[location] != " "
 
-    puts win_combination
+    puts WIN_COMBINATIONS[0]
 
   end
 
