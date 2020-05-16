2af8a88e17027f9e714901533b12c5459938a44c
Molly Cooper <mollycooper64@gmail.com>
2020-05-16 18:38:09 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,12 +17,17 @@ WIN_COMBINATIONS = [
 
 def won?(board)
 
-  WIN_COMBINATIONS.detect do | win_combination |
+  if board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+    false
+
+  else  WIN_COMBINATIONS.detect do | win_combination |
     location1 = win_combination[0]
     location2 = win_combination[1]
     location3 = win_combination[2]
 
     board[location1] == board[location2] && board[location2] == board[location3] && board[location] != " "
   end
+
+end
 
 end
